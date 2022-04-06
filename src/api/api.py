de9a721ffa4c9cfc2b87dcaddbacaf94afbbdc0f
Aurimas Racas <mail@aurimas.eu>
2022-04-06 15:53:01 -0400

    Changed all API endpoints to skip FastAPI validation Speed of API responses up to 50%

--- src/api/api.py
+++ src/api/api.py
@@ -1,17 +1,46 @@
-from fastapi import FastAPI, Body
+
 import psycopg2 as pg
 from psycopg2.extras import DictCursor
-
 import asyncio
-from pydantic import BaseModel
+
 from enum import Enum
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Any
 import sys
 sys.path.append("../")
+
 
 import isochrones as isc
 from sqlalchemy import create_engine
 from shapely.geometry import mapping
+
+
+#please see @ https://github.com/tiangolo/fastapi/issues/1359#issuecomment-927789546 on what we are using to speed up FastAPI
+from fastapi import FastAPI, Body, status
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel as PydanticBaseModel
+import orjson
+from bson import ObjectId
+
+def orjson_dumps(v, *, default):
+    # orjson.dumps returns bytes, to match standard json.dumps we need to decode
+    return orjson.dumps(v, default=default, option=orjson.OPT_NON_STR_KEYS).decode()
+
+
+class BaseModel(PydanticBaseModel):
+    class Config:
+        json_load = orjson.loads
+        json_dumps = orjson_dumps
+        json_encoders = {ObjectId: lambda x: str(x)}
+
+class PydanticJSONResponse(JSONResponse):
+    def render(self, content: Any) -> bytes:
+        if content is None:
+            return b""
+        if isinstance(content, bytes):
+            return content
+        if isinstance(content, BaseModel):
+            return content.json(by_alias=True).encode(self.charset)        
+        return content.encode(self.charset)
 
 class backendApi:
     def __init__(self, db_params, otp_port = 8062) -> None:
@@ -114,26 +143,31 @@ class backendApi:
             name: str
             h3id: str
             category: str
-            coords: coordinates        
+            coords: coordinates
 
-        @app.get("/pois/{city_id}/{poi_category}", response_model = List[POI])
-        async def get_pois_in_city(city_id: int, poi_category: str):
+        class POIList(BaseModel):
+            data: List[POI]
+
+        @app.get("/pois/{city_id}/{poi_category}", response_model = POIList)
+        async def get_pois_in_city(city_id: int, poi_category: str, native: bool = False):
             """Get all POIs of a given category in a city.
             """
             with self.get_db_connection() as conn:
                 with conn.cursor(cursor_factory=DictCursor) as cur:
                     sql = 'SELECT id, h3id, name, lat, long, category FROM api_get_pois_for_city(%s, %s)'                    
                     cur.execute(sql, (city_id, poi_category))
-                    response = []
-                    while True:
-                        row = cur.fetchone()  
-                        if row is None:
-                            break                      
+                    response = POIList.construct(data = [])
+                    data = cur.fetchall()                  
+                    for row in data:                        
                         obj = dict(row)             
                         #remap lat/long into coordinates
                         obj['coords'] = coordinates(lat = row['lat'], long=row['long'])
-                        response.append(POI(**obj))                         
-            return response
+                        response.data.append(POI.construct(**obj))  
+            
+            if native:
+                return response
+            else:                       
+                return PydanticJSONResponse(content=response)
 
         
         class H3Grid(BaseModel):
@@ -141,8 +175,11 @@ class backendApi:
             data: Optional[dict]
             total: float
 
-        @app.get("/demographics/{city_id}/{demographics_category}", response_model=List[H3Grid])
-        async def get_city_demographics(city_id: int, demographics_category: str, detailed: int = 0):
+        class H3List(BaseModel):
+            data: List[H3Grid]        
+
+        @app.get("/demographics/{city_id}/{demographics_category}", response_model=H3List)
+        async def get_city_demographics(city_id: int, demographics_category: str, detailed: int = 0, native: bool = False):
             """ Returns requested demographic data for all H3 cells in the city. 
             Use detailed=False to retrieve totals per h3cell for initial drawing and detailed=True to get details by category for tooltips.
             """
@@ -155,17 +192,15 @@ class backendApi:
                         sql = "SELECT h3id, 'total' as groupname, SUM(population) as population from api_get_demographics_for_city(%s, %s) GROUP BY h3id"
                     cur.execute(sql, (city_id, demographics_category))
                     response = {}
-                    while True:
-                        row = cur.fetchone()  
-                        if row is None:
-                            break                    
-                        
+                    data = cur.fetchall()
+
+                    for row in data:                        
                         #check if H3cell does not exist yet - create then
                         id = row['h3id']
                         if id not in response:
                             total = 0 if detailed else row['population']
                             data = {} if detailed else None
-                            h3_obj = H3Grid(h3id = id, data = data, total=total)                            
+                            h3_obj = H3Grid.construct(h3id = id, data = data, total=total)                            
                             response[id] = h3_obj
                         
                         #add population data
@@ -174,8 +209,13 @@ class backendApi:
                             population = row['population']
                             response[id].data[groupname] = population
                             response[id].total += population
+                    
+                    response = H3List.construct(data = list(response.values()))
                         
-            return list(response.values())
+            if native:
+                return response
+            else:                       
+                return PydanticJSONResponse(content=response)
 
         
         class MultiPolygon(BaseModel):
@@ -204,13 +244,13 @@ class backendApi:
             
             population_details = {row['groupname']: row['population'] for row in data}
             population_total = sum(population_details.values())
-            area = CatchmentArea(
+            area = CatchmentArea.construct(
                 geometry = mapping(isochrone), 
                 origin_h3id = origin_h3id,
                 population_total = population_total,
                 population_detail = population_details,
             )
-            return area
+            return PydanticJSONResponse(content=area)
 
         class CityStats(BaseModel):
             index_total: float
@@ -239,8 +279,8 @@ class backendApi:
             poi_list: List[str]
 
         class CityData(BaseModel):
-            demographics: Optional[List[H3Grid]]
-            pois: Optional[List[POI]]
+            demographics: Optional[H3List]
+            pois: Optional[POIList]
             stats: Optional[CityStats]
 
 
@@ -249,10 +289,19 @@ class backendApi:
             queries = {}
 
             if DataFields.demographics in update_pack.changed:
-                queries['demographics'] = get_city_demographics(city_id=city_id, demographics_category=update_pack.config.demographic_category, detailed=True)
+                queries['demographics'] = get_city_demographics(
+                    city_id=city_id, 
+                    demographics_category=update_pack.config.demographic_category, 
+                    detailed=True,
+                    native=True
+                )
             
             if DataFields.pois in update_pack.changed:
-                queries['pois'] = get_pois_in_city(city_id = city_id, poi_category= update_pack.config.poi_category)
+                queries['pois'] = get_pois_in_city(
+                    city_id = city_id, 
+                    poi_category= update_pack.config.poi_category,
+                    native=True
+                )
                         
             if len(update_pack.changed) > 0:
                 queries['stats'] = get_city_accessibility_statistics(
@@ -261,10 +310,9 @@ class backendApi:
                     time_of_day = update_pack.config.time_of_day, 
                     poi_category = update_pack.config.poi_category
                 )
-                
-            
+                            
             results = await asyncio.gather(*queries.values())            
             dict_results = dict(zip(queries.keys(), results))
-            return CityData(** dict_results)
+            return PydanticJSONResponse(content=CityData.construct(** dict_results))
             
         return app
