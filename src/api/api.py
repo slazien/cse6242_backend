45e939be07cf1923f6a0f9920e7d39ed5bbe2303
Aurimas Racas <mail@aurimas.eu>
2022-04-11 18:21:18 -0400

    Implementation of city stats API endpoint

--- src/api/api.py
+++ src/api/api.py
@@ -149,13 +149,17 @@ class backendApi:
             data: List[POI]
 
         @app.get("/pois/{city_id}/{poi_category}", response_model = POIList)
-        async def get_pois_in_city(city_id: int, poi_category: str, native: bool = False):
+        async def get_pois_in_city(city_id: int, poi_category: str, native: bool = False, pois_to_exclude = []):
             """Get all POIs of a given category in a city.
             """
             with self.get_db_connection() as conn:
                 with conn.cursor(cursor_factory=DictCursor) as cur:
-                    sql = 'SELECT id, h3id, name, lat, long, category FROM api_get_pois_for_city(%s, %s)'                    
-                    cur.execute(sql, (city_id, poi_category))
+                    if pois_to_exclude:
+                        sql = 'SELECT id, h3id, name, lat, long, category FROM api_get_pois_for_city(%s, %s) WHERE h3id NOT IN %s'                    
+                        cur.execute(sql, (city_id, poi_category, tuple(pois_to_exclude)))
+                    else:
+                        sql = 'SELECT id, h3id, name, lat, long, category FROM api_get_pois_for_city(%s, %s)'                    
+                        cur.execute(sql, (city_id, poi_category))
                     response = POIList.construct(data = [])
                     data = cur.fetchall()                  
                     for row in data:                        
@@ -265,7 +269,8 @@ class backendApi:
             poi_category, 
             pois_added = [],
             pois_removed = []
-        ):            
+        ):  
+            """Returns overall city accessibility statistics and a breakdown by demographics category"""          
             with self.get_db_connection() as conn:                
                 with conn.cursor(cursor_factory=DictCursor) as cur:                    
                     sql = 'SELECT groupn, metric, population FROM api_get_city_stats(%s, %s, %s, %s, %s, %s)'                    
@@ -275,7 +280,7 @@ class backendApi:
                     details = {d['groupn'] : d['metric'] for d in data}
                     total = sum(d['metric'] * d['population'] for d in data) / sum([d['population'] for d in data])
 
-            """Returns overall city accessibility statistics and a breakdown by demographics category"""
+            
             return CityStats(index_total = total, index_detail = details)
 
         class ConfigSet(BaseModel):
@@ -316,19 +321,22 @@ class backendApi:
                     native=True
                 )
             
-            if DataFields.pois in update_pack.changed:
+            if DataFields.pois in update_pack.changed or update_pack.poi_list.deleted:
                 queries['pois'] = get_pois_in_city(
                     city_id = city_id, 
                     poi_category= update_pack.config.poi_category,
-                    native=True
+                    native=True,
+                    pois_to_exclude=update_pack.poi_list.deleted
                 )
                         
-            if len(update_pack.changed) > 0:
+            if update_pack.changed or update_pack.poi_list.added or update_pack.poi_list.deleted:
                 queries['stats'] = get_city_accessibility_statistics(
                     city_id=city_id, 
                     demographics_category=update_pack.config.demographic_category, 
                     time_of_day = update_pack.config.time_of_day, 
-                    poi_category = update_pack.config.poi_category
+                    poi_category = update_pack.config.poi_category,
+                    pois_added=update_pack.poi_list.added,
+                    pois_removed=update_pack.poi_list.deleted
                 )
                             
             results = await asyncio.gather(*queries.values())            
